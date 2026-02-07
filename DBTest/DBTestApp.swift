dae2b3312dce5cc62b3a56916f48f07aa8a20565
songdealer <newmin123@naver.com>
2026-02-07 21:53:48 +0900

    Initial Commit

---
+++ DBTest/DBTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DBTestApp.swift
+//  DBTest
+//
+//  Created by user on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct DBTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DBTestTests/DBTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  DBTestTests.swift
+//  DBTestTests
+//
+//  Created by user on 2/7/26.
+//
+
+import Testing
+@testable import DBTest
+
+struct DBTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
